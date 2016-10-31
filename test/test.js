38aa04d9b64ec48731e1dcc66651b1a85ea2574f
Neil Sarkar <neil.r.sarkar@gmail.com>
2016-10-31 17:22:20 +0100

    Provide tests for 2xx responses

--- test/test.js
+++ test/test.js
@@ -10,8 +10,8 @@ describe('http2human', function() {
   before(function() {
     app.get('/json200', function(req, res) { res.send(JSON.stringify({cool: 'nice'})); });
     app.get('/text200', function(req, res) { res.send('nice'); });
-    app.get('/json201', function(req, res) { res.send(JSON.stringify({cool: 'nice'})); });
-    app.get('/text201', function(req, res) { res.send(JSON.stringify({cool: 'nice'})); });
+    app.get('/json201', function(req, res) { res.send(JSON.stringify({created: true})); });
+    app.get('/text201', function(req, res) { res.sendStatus(201) });
 
     app.listen(port);
   })
@@ -27,6 +27,20 @@ describe('http2human', function() {
     it('returns text if response is text', function(done) {
       h2h('http://localhost:6969/text200').then(function(response) {
         expect(response).toEqual('nice');
+        done();
+      }).catch(done);
+    })
+
+    it('parses json for 201', function(done) {
+      h2h('http://localhost:6969/json201').then(function(response) {
+        expect(response.created).toExist();
+        done();
+      }).catch(done);
+    });
+
+    it('returns text for 201', function(done) {
+      h2h('http://localhost:6969/text201').then(function(response) {
+        expect(response).toEqual('Created');
         done();
       }).catch(done);
     })
